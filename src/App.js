ce620d59f617c9f10dd4a68468312489745e826e
Hasnain-Shabbir <hasnainshabbir186@gmail.com>
2022-07-17 03:08:25 +0500

    Create the layout of Navbar.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import { BrowserRouter as Routes, Route, Link } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import { layout, Typography, Space } from "antd";
 
+// Components
+import { Navbar } from "./components";
+
+// Styles
 import "./App.css";
 
 function App() {
   return (
-    <Routes>
-      <div className="App">
-        <div className="navbar">navbar</div>
-        <div className="main">main</div>
-        <div className="footer">footer</div>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
       </div>
-      ;
-    </Routes>
+      <div className="main">main</div>
+      <div className="footer">footer</div>
+    </div>
   );
 }
 
