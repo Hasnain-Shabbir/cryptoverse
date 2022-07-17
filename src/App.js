ce33d0468ea9ae54d8ac82620c3bdfeab410a8c3
Hasnain-Shabbir <hasnainshabbir186@gmail.com>
2022-07-17 11:44:43 +0500

    Create the Footer

--- src/App.js
+++ src/App.js
@@ -45,9 +45,23 @@ function App() {
             </Routes>
           </div>
         </Layout>
-      </div>
 
-      <div className="footer">footer</div>
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            Cryptoverse <br />
+            All rights reserved.
+          </Typography.Title>
+
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
     </div>
   );
 }
