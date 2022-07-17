77b72d1700eaf649ece25361177efe6e8b975e70
Hasnain-Shabbir <hasnainshabbir186@gmail.com>
2022-07-17 12:16:21 +0500

    Setup the Homepage.

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import millify from "millify";
+import { Typography, Row, Col, Statistic } from "antd";
+
+const { Title } = Typography;
 
 function Homepage() {
-  return <div>Homepage</div>;
+  return (
+    <React.Fragment>
+      <Title level={2} className="heading">
+        Global Crypto Stats
+      </Title>
+      <Row>
+        <Col span={12}>
+          <Statistic title="Total Cryptocurrencies" value="5" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Exchanges" value="5" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Market Cap" value="5" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total 24h Volume" value="5" />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Markets" value="5" />
+        </Col>
+      </Row>
+    </React.Fragment>
+  );
 }
 
 export default Homepage;
