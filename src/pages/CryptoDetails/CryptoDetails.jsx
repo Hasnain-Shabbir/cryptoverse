58c2f6c24b233a910869c6f24ece6887718032f0
Hasnain-Shabbir <hasnainshabbir186@gmail.com>
2022-07-18 21:15:03 +0500

    Fix the a tag issue.

--- src/pages/CryptoDetails/CryptoDetails.jsx
+++ src/pages/CryptoDetails/CryptoDetails.jsx
@@ -169,7 +169,7 @@ const CryptoDetails = () => {
                 <Title level={5} className="link-name">
                   {link.type}
                 </Title>
-                <a href={link.url} target="_blank" rel="noreffer">
+                <a href={link.url} target="_blank" rel="noreferrer">
                   {link.name}
                 </a>
               </Row>
